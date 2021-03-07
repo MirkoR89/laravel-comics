2e2afef3617948f02eced6a11de7e9ca80065226
Mirko <mirko.rinelli@gmail.com>
2021-03-07 19:05:17 +0100

    Header style - Navbar top and bottom

--- resources/views/admin/Comics/index.blade.php
+++ resources/views/admin/Comics/index.blade.php
@@ -1,30 +1,54 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<h2>Comics</h2>
-<a class="btn btn-primary mb-3" href="{{ route('admin.comics.create') }}" role="button">Add comics</a>
+<h1>Comics</h1>
 
-<div class="d-flex">
+{{-- Button create --}}
+<a class="btn btn-primary my-4" href="{{ route('admin.comics.create') }}" role="button">Add comics</a>
+
+{{-- Comics card --}}
+<div class="d-flex flex-wrap">
     @foreach ($comics as $comic)
-    <div class="card" style="width: 22rem;">
+    <div class="card m-3" style="width: 20rem;">
         @if ('cover')
         <img src="{{ asset('storage/' . $comic->cover ) }}" class="card-img-top" alt="...">
         @endif
         <div class="card-body">
             <h5 class="card-title">{{$comic->title}}</h5>
-            <p class="card-text">{{$comic->description}}</p>
-            <span class="card-text">{{$comic->price}}</span>
-            <a href="{{ route('admin.comics.show', ['comic'=>$comic->id]) }}" class="btn btn-success">Show</a>
+            <h6><small class="text-muted">{{$comic->series}}</small></h6>
 
-            <form action="{{ route('admin.comics.destroy', ['comic'=>$comic->id]) }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">
-                    Delete
-                </button>
-            </form>
+            <ul class="list-group list-group-flush">
+                {{-- Available --}}
+                @if ($comic->available == 0)
+                <li class="list-group-item">NOT AVAILABLE</li>
+                @else
+                <li class="list-group-item">AVAILABLE</li>
+                @endif
 
-            {{-- Modal --}}
+                {{-- Us price --}}
+                <li class="list-group-item">Us price: $ {{$comic->price}}</li>
+                
+                {{-- On sale date --}}
+                <li class="list-group-item">On sale date: {{$comic->on_sale_date}}</li>
+            </ul>
+
+            {{-- Buttons --}}
+            <div class="card-body d-flex justify-content-around">
+                {{-- Button show--}}
+                <a href="{{ route('admin.comics.show', ['comic'=>$comic->id]) }}"
+                    class="card-link btn btn-success">Show</a>
+
+                {{-- Button delete--}}
+                <form action="{{ route('admin.comics.destroy', ['comic'=>$comic->id]) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="card-link btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+            </div>
+
+            {{-- Modal
             <div class="modal fade" id="destroy-{{$comic->id}}" tabindex="-1" role="dialog"
                 aria-labelledby="comic-destroy-{{$comic->id}}" aria-hidden="true">
                 <div class="modal-dialog" role="document">
@@ -47,7 +71,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
     @endforeach
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,64 @@
+<header>
+
+    {{-- Navbar top --}}
+    <nav class="navtop fluid-container">
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="#"><img src="{{ asset('img/DC_desktop_blue.svg') }}" alt=""></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><img src="{{ asset('img/DCUI_desktop.svg') }}" alt=""></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><img src="{{asset('img/DC_SHOP_desktop.svg')}}" alt=""></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><img src="{{asset('img/DC_community.svg')}}" alt=""></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><img src="{{asset('img/DC_on_HBOMAX_desktop.svg')}}" alt=""></a>
+            </li>
+        </ul>
+    </nav>
+
+    {{-- Navbar bottom --}}
+    <nav class="navbottom fluid-container">
+        <div class="d-flex align-items-center justify-content-around">
+            <div class="log">
+                <a class="nav-link active" href="#"><img src="{{ asset('img/logo.png') }}" alt=""></a>
+            </div>
+            <ul class="nav align-items-center justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">characters</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">comics</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">movies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">tv</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">games</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">videos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">news</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">shop <i class="fas fa-caret-down"></i></a>
+                </li>
+            </ul>
+            <div class="search">
+                <form action="search" method="get">
+                    <input type="search" name="search">
+                    <span>search <i class="fas fa-search"></i></span>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
